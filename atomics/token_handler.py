04f0e3f2b738396c3410faf597857e3059323ac4
fpresenza <francisco.presenza@gmail.com>
2024-04-05 18:48:56 -0300

    TokenHandler class definition

---
+++ atomics/token_handler.py
@@ -0,0 +1,33 @@
+import numpy as np
+from dataclasses import dataclass
+from pypdevs.DEVS import AtomicDEVS
+
+
+@dataclass
+class Token:
+    creator: str                # The robot that created it
+    order: int                  # A counter to differentiate tokens
+    data: object                # The data it carries
+    hops_to_target: int         # The number of hops it must travel
+    hops_travelled: int = 0     # The number of hops it has travelled
+
+
+class TokenHandler(AtomicDEVS):
+    def __init__(self, name=None):
+        """Atomic model for the toking handling protocol"""
+
+        # Always call parent class' constructor FIRST:
+        AtomicDEVS.__init__(self, name)
+
+        # Dictionary used to remember the last tokens retransmitted
+        # {cretor: order}
+        self.retransmitted = {}
+
+        # PORTS:
+        #  Declare as many input and output ports as desired
+        #  (usually store returned references in local variables):
+        self.OUT_token   = self.addOutPort(name="token_out")
+        self.OUT_control = self.addOutPort(name="control_out")
+        self.IN_token    = self.addInPort(name="token_in")
+        self.IN_control  = self.addInPort(name="control_in")
+        self.IN_state    = self.addInPort(name="state_in")
