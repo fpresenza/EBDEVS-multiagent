1fa40dd03a6ffc3ce007ec6861066a1727f29777
fpresenza <francisco.presenza@gmail.com>
2024-05-10 18:51:24 -0300

    controller generator

--- atomics/controller.py
+++ atomics/controller.py
@@ -3,6 +3,101 @@ from pypdevs.DEVS import AtomicDEVS
 from pypdevs.infinity import INFINITY
 
 
+class ControllerGeneratorState:
+    """
+    Encapsulates the system's state
+    """
+
+    def __init__(self, sigmaval=0.1, tval=0.0):
+        """
+        Constructor (parameterizable).
+        """
+        self.set(sigmaval, tval)
+
+    def set(self, sigmavalue, tvalue, ivalue):
+        self._sigma  = sigmavalue
+        self._tvalue = tvalue
+
+    def get(self):
+        return self._sigma, self._tvalue
+
+
+class ControllerGenerator(AtomicDEVS):
+    def __init__(self,name=None,period=1):
+        """
+        Atomic model for the controller inputs
+        """
+
+        # Always call parent class' constructor FIRST:
+        AtomicDEVS.__init__(self, name)
+
+        # STATE:
+        #  Define 'state' attribute (initial sate):
+        _time0  = 0.0
+        _sigma0 = 0
+        self.state = ControllerGeneratorState(_sigma0,_time0) 
+        # ELAPSED TIME:
+        #  Initialize 'elapsed time' attribute if required
+        #  (by default, value is 0.0):
+        self.elapsed = 0.0
+
+        # PORTS:
+        #  Declare as many input and output ports as desired
+        #  (usually store returned references in local variables):
+        self.out_controller_extact = self.addOutPort(name="out_controller_extact")
+        self.out_controller_extpos = self.addOutPort(name="out_controller_extpos")
+        self.out_controller_intpos = self.addOutPort(name="out_controller_intpos")
+ 
+        # Parameters
+        self.msgs = [
+            {self.out_controller_extact: np.array([-30.0, -30.0])},
+            {self.out_controller_extpos: np.array([30.0, 30.0])},
+            {self.out_controller_intpos: np.array([40.0, 50.0])}
+        ]
+        self.period = period
+
+    def __lt__(self, other):
+        return self.name < other.name
+
+    def extTransition(self, inputs):
+        """
+        External Transition Function.
+        """
+        # it should never be executed
+        sigma, current_time, i = self.state.get()
+        current_time += self.elapsed
+        return ControllerGeneratorState(sigma,current_time) 
+    
+    def intTransition(self):
+        """
+        Internal Transition Function.
+        """
+        sigma, current_time = self.state.get()
+        current_time += sigma
+        self.msgs.pop()
+        if len(self.msgs) == 0:
+            sigma = INFINITY
+        else:
+            sigma = self.period
+        return ControllerGeneratorState(sigma,current_time) 
+    
+    def outputFnc(self):
+        """
+        Output Funtion.
+        """
+        # sigma, current_time = self.state.get()
+        return self.msgs[-1]
+
+    def timeAdvance(self):
+        """
+        Time-Advance Function.
+        """
+        # Compute 'ta', the time to the next scheduled internal transition,
+        # based (typically) on current State.
+        sigma, current_time, i = self.state.get()
+        return sigma
+
+
 class ControllerState:
     """
     Encapsulates the system's state
@@ -52,7 +147,8 @@ class Controller(AtomicDEVS):
             _time0, 
             _last_position0,
             _ext_action0
-        ) 
+        )
+        self.subframework = {}
         # ELAPSED TIME:
         #  Initialize 'elapsed time' attribute if required
         #  (by default, value is 0.0):
@@ -82,7 +178,8 @@ class Controller(AtomicDEVS):
             last_position = inputs[self.in_kalman_intpos]
             sigma = sigma - self.elapsed # holds last status
         elif self.in_handler_extpos in inputs: # if ext pos arrives through port IN_handler
-            # TODO: agregar position a subframework
+            node_id, ext_position = self.in_handler_extpos
+            self.subframework[node_id] = ext_position
         elif self.in_handler_extact in inputs: # if ext action arrives through port IN_handler
             ext_action += inputs[self.in_handler_extact]
             sigma = sigma - self.elapsed # holds last status
