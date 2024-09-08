2ebb05842b9f98b35defdb83be2ea0d1952de766
fpresenza <francisco.presenza@gmail.com>
2024-09-08 19:09:46 -0300

    fix animation timestep

--- xy_animation.py
+++ xy_animation.py
@@ -41,55 +41,65 @@ robot_ids = summary['robot_ids']
 n = len(robot_ids)
 print('Experiment with {} robots'.format(n))
 
-frames = []
-positions = np.zeros((n, 2), dtype=float)
-edges = []
-timesteps = [[] for _ in robot_ids]
-with open('output/data.csv', 'r') as file:
-    reader = csv.reader(file)
-    for row in reader:
-        robot_index = robot_ids.index(row[0])
-        time = float(row[1])
-        timesteps[robot_index].append(time)
-        positions[robot_index] = row[2:4]
-        neighbors = [int(r[-1]) for r in row[5:]]
-
-        edges = [e for e in edges if robot_index not in e] + \
-            [[robot_index, neighbor] for neighbor in neighbors]
-        teams = np.array([1, 2, 3, 4])
-        
-        frames.append([time, positions.copy(), edges, teams])
-
-
-nodes = np.arange(n)
-
 # ------------------------------------------------------------------
 # Create Frames
 # ------------------------------------------------------------------
-lim = 15.0
-timestep = 0.01
-print([len(ts) for ts in timesteps])
+data = []
+with open('output/data.csv', 'r') as file:
+    reader = csv.reader(file)
+    for row in reader:
+        data.append(row)
+
+frames = []
+positions = np.zeros((n, 2), dtype=float)
+edges = []
+for step in range(len(data[:-1])):
+    robot_index = robot_ids.index(data[step][0])
+    time = float(data[step][1])
+    positions[robot_index] = data[step][2:4]
+    neighbors = [int(r[-1]) for r in data[step][5:]]
+
+    edges = [e for e in edges if robot_index not in e] + \
+        [[robot_index, neighbor] for neighbor in neighbors]
+
+    if time < float(data[step + 1][1]):
+        teams = np.array([1, 2, 3, 4])
+        frames.append([time, positions.copy(), edges, teams])
+
+print('Total number of frames: {}'.format(len(frames)))
+frames = frames[::10]
+print('Reduced number of frames: {}'.format(len(frames)))
+timestep = np.diff([frame[0] for frame in frames])
+print('Average time between frames  Q_1={:.5f} sec, Q_2={:.5f} sec, Q_3={:.5f} sec'.format(
+    np.quantile(timestep, 0.25),
+    np.quantile(timestep, 0.50),
+    np.quantile(timestep, 0.75),
+))
+
+
+timestep = np.quantile(timestep, 0.50)
+print('Animation time between frames: {:.5f} sec'.format(timestep))
+print('Animation total duration: {:.5f} sec'.format(len(frames) * timestep))
 
 # ------------------------------------------------------------------
 # Plot vs time
 # ------------------------------------------------------------------
 fig, ax = plt.subplots(2, 1, figsize=(10, 8))
-ax[0].set_xlabel('time [$seg$]')
+ax[0].set_xlabel('time [$sec$]')
 ax[0].set_ylabel('$x$-position [$m$]')
 ax[0].grid(1)
 ax[0].plot(
     [f[0] for f in frames], [f[1][:, 0] for f in frames],
-    marker='.',
+    # marker='.',
     ds='steps-post')
-ax[1].set_xlabel('time [$seg$]')
+ax[1].set_xlabel('time [$sec$]')
 ax[1].set_ylabel('$y$-position [$m$]')
 ax[1].grid(1)
 ax[1].plot(
-    [f[0] for f in frames], [f[1][:, 0] for f in frames],
-    marker='.',
+    [f[0] for f in frames], [f[1][:, 1] for f in frames],
+    # marker='.',
     ds='steps-post')
-fig.savefig('/tmp/position.png', format='png', dpi=360)
-plt.show()
+fig.savefig('position.png', format='png', dpi=360)
 
 # ------------------------------------------------------------------
 # Animation
@@ -105,6 +115,7 @@ ax.set_aspect('equal')
 # ax.grid(1, lw=0.4)
 ax.set_xlabel(r'$x$ [m]', fontsize='small', labelpad=0.6)
 ax.set_ylabel(r'$y$ [m]', fontsize='small', labelpad=0.6)
+lim = 15.0
 ax.set_xlim(-lim, lim)
 ax.set_ylim(-lim, lim)
 ax.grid(1)
@@ -168,4 +179,4 @@ anim.ax.legend(
     fontsize='small',
     handletextpad=1
 )
-# anim.run('xy_animation.mp4')
+anim.run('xy_animation.mp4')
