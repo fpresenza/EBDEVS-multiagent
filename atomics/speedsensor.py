8db84db1b2ca972c7f403cd763a2d8a12f14e273
Eze <emarcosig@dc.uba.ar>
2024-09-13 18:32:54 -0300

    Added sensor atomic model to Robot Dynamics coupled model.

---
+++ atomics/speedsensor.py
@@ -0,0 +1,112 @@
+import numpy as np
+from pypdevs.DEVS import AtomicDEVS
+from pypdevs.infinity import INFINITY
+
+class SpeedSensorState:
+    """
+    Encapsulates the system's state
+    """
+
+    def __init__(self, sigmaval=0.1, tval=0.0, dataval=[]):
+        """
+        Constructor (parameterizable).
+        """
+        self.set(sigmaval, tval, dataval)
+
+    def set(self, sigmavalue, tvalue, datavalue):
+        self._sigma  = sigmavalue
+        self._tvalue = tvalue
+        self._data   = datavalue
+
+    def get(self):
+        return self._sigma, self._tvalue, self._data
+
+
+class SpeedSensor(AtomicDEVS):
+    def __init__(self,name=None,noisestd=0.0,bias=np.zeros((2,1)),trans=np.eye(2),debug=False):
+        """Atomic model for the speed sensor"""
+
+        # Always call parent class' constructor FIRST:
+        AtomicDEVS.__init__(self, name)
+
+        # Parameters
+        # self.robot_id = robot_id    # Robot identifier
+        # self.status = []          # TODO
+
+        # STATE:
+        #  Define 'state' attribute (initial sate):
+        _time0  = 0.0
+        _sigma0 = INFINITY # waits till firts token
+        _data0  = []
+        self.state = SpeedSensorState(_sigma0,_time0,_data0) 
+        # ELAPSED TIME:
+        #  Initialize 'elapsed time' attribute if required
+        #  (by default, value is 0.0):
+        self.elapsed = 0.0
+
+        self.noisestd = noisestd
+        self.bias     = bias
+        self.trans    = trans
+
+        self.debug = debug
+
+        # PORTS:
+        #  Declare as many input and output ports as desired
+        #  (usually store returned references in local variables):
+        self.out_measured_speed = self.addOutPort(name="out_measured_speed")
+        #
+        self.in_commanded_speed = self.addInPort(name="in_commanded_speed")
+
+        if (self.debug):
+            print("t: 0 s, Atomic name: {}, Init Function".format(self.name))
+
+    def __lt__(self, other):
+        return self.name < other.name
+    
+    def extTransition(self, inputs):
+        """
+        External Transition Function.
+        """
+        sigma, current_time, _ = self.state.get()
+        current_time += self.elapsed
+
+        #if self.in_commanded_speed in inputs: # if data arrives through port in_control_intact
+        v = inputs[self.in_commanded_speed].reshape(2,1)
+        noise = np.random.normal(loc=self.bias,scale=self.noisestd,size=(2,1))
+        vmeasured = self.trans.dot(v + noise)
+        data = [{self.out_measured_speed: vmeasured}]
+        sigma = 0 # holds last status
+        
+        if (self.debug):
+            print("t: {:.2f} s, Parent name: {}, Atomic name: {}, External Transition Function, v: {}, vmeasured: {}".format(current_time,self.parent.parent.name,self.name,v,vmeasured))
+
+        return SpeedSensorState(sigma, current_time, data) 
+    
+    def intTransition(self):
+        """
+        Internal Transition Function.
+        """
+        _, current_time, data = self.state.get()
+        data.pop()
+        sigma = INFINITY
+
+        if (self.debug):
+            print("t: {:.2f} s, Atomic name: {}, Internal Transition Function".format(current_time,self.name))
+            
+        return SpeedSensorState(sigma,current_time,data) 
+    
+    def outputFnc(self):
+        """
+        Output Funtion.
+        """
+        sigma, current_time, data = self.state.get()
+        return data[-1]
+
+    def timeAdvance(self):
+        """
+        Time-Advance Function.
+        """
+        # Compute 'ta', the time to the next scheduled internal transition,
+        # based (typically) on current State.
+        sigma, _, _ = self.state.get()
+        return sigma
--- multirobot_ebdevs.py
+++ multirobot_ebdevs.py
@@ -24,6 +24,7 @@ from atomics.router import *
 from atomics.controller import Controller
 from atomics.token_handler import TokenHandler
 from atomics.kalman_filter import KalmanFilter
+from atomics.speedsensor import SpeedSensor
 
 from utils import (
     read_json_file,
@@ -197,13 +198,21 @@ class RobotDynamics(CoupledDEVS):
                                          x0=self.y0, 
                                          debug=self.debug
                                          )
+        speed_sensor = SpeedSensor(name="vmeas",
+                                   noisestd=0.0,
+                                   bias=np.ones((2,1)),
+                                   trans=np.eye(2),
+                                   debug=True
+                                   )
         self.splitter     = self.addSubModel(splitter)
         self.integrator_x = self.addSubModel(integrator_x)
         self.integrator_y = self.addSubModel(integrator_y)
+        self.speed_sensor = self.addSubModel(speed_sensor)
 
         # Declare the coupled model's output ports:
         self.OUT_dynamics_x    = self.addOutPort(name="OUT_dynamics_x")
         self.OUT_dynamics_y    = self.addOutPort(name="OUT_dynamics_y")
+        self.OUT_measured_v    = self.addOutPort(name="OUT_measured_v")
         self.IN_dynamics_vx_vy = self.addInPort( name="IN_dynamics_vx_vy")
 
         # Connect coupled model's ports with atomic models' ports
@@ -212,6 +221,8 @@ class RobotDynamics(CoupledDEVS):
         self.connectPorts(self.splitter.out_splitter_msgs[1], self.integrator_y.IN_dx)
         self.connectPorts(self.integrator_x.OUT_q, self.OUT_dynamics_x)
         self.connectPorts(self.integrator_y.OUT_q, self.OUT_dynamics_y)
+        self.connectPorts(self.IN_dynamics_vx_vy, self.speed_sensor.in_commanded_speed)
+        self.connectPorts(self.speed_sensor.out_measured_speed, self.OUT_measured_v)
 
         if (self.debug):
             print("t: 0 s, Coupled name: {}, Init Function".format(self.name))
