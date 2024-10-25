41fd16508403b6a8bb0df3ccbeddcaac68c584c6
fpresenza <francisco.presenza@gmail.com>
2024-10-25 20:12:25 -0300

    map: robot_id -> index

--- utils.py
+++ utils.py
@@ -34,3 +34,7 @@ def find_latest_timestamp(parent_path):
 
     return latest + '/'
 
+
+def robot_id_to_index(robot_id):
+    underscore = robot_id.rindex('_')
+    return int(robot_id[underscore + 1:])
