6ae8303c2ac616a009f0f1083b4ea7ba020c6372
fpresenza <francisco.presenza@gmail.com>
2024-10-29 12:38:07 -0300

    wip: improve names within controller

--- atomics/controller.py
+++ atomics/controller.py
@@ -13,24 +13,24 @@ class ControllerState:
 
     def __init__(
             self, 
-            sigmaval=0.1, 
-            tval=0.0, 
+            sigma=0.1, 
+            tvalue=0.0, 
             last_position=np.array([0.0, 0.0]), 
-            ext_action=np.array([0.0, 0.0])
+            external_action=np.array([0.0, 0.0])
         ):
         """
         Constructor (parameterizable).
         """
-        self.set(sigmaval, tval, last_position, ext_action)
+        self.set(sigma, tvalue, last_position, external_action)
 
-    def set(self, sigmavalue, tvalue, last_position, control_action):
-        self._sigma  = sigmavalue
+    def set(self, sigma, tvalue, last_position, external_action):
+        self._sigma  = sigma
         self._tvalue = tvalue
         self._last_position = last_position
-        self._ext_action = control_action
+        self._external_action = external_action
 
     def get(self):
-        return self._sigma, self._tvalue, self._last_position, self._ext_action
+        return self._sigma, self._tvalue, self._last_position, self._external_action
 
 
 class Controller(AtomicDEVS):
@@ -46,21 +46,13 @@ class Controller(AtomicDEVS):
 
         # STATE:
         #  Define 'state' attribute (initial sate):
-        _time0  = 0.0
-        _sigma0 = self.period # waits till firts token
-        _last_position0 = None
-        _ext_action0 = np.array([0.0, 0.0])
         self.state = ControllerState(
-            _sigma0,
-            _time0, 
-            _last_position0,
-            _ext_action0
+            sigma=self.period,   # waits till first token
+            tvalue=0.0, 
+            last_position=None, 
+            external_action=np.zeros(2, dtype=float)
         )
         self.subframework = {}
-
-        self.freq   = 0.05
-        self.radius = np.random.uniform(1.0, 10.0)
-        self.omega  = np.random.choice([-1, 1]) * 2*np.pi*self.freq
 
         # ELAPSED TIME:
         #  Initialize 'elapsed time' attribute if required
@@ -89,54 +81,57 @@ class Controller(AtomicDEVS):
         """
         External Transition Function.
         """
-        sigma, current_time, last_position, ext_action = self.state.get()
+        sigma, current_time, last_position, external_action = self.state.get()
         current_time += self.elapsed
 
         if self.in_kalman_intpos in inputs: # if data arrives through port in_kalman_intpos
             last_position = inputs[self.in_kalman_intpos]
             sigma = sigma - self.elapsed # holds last status
         elif self.in_handler_extpos in inputs: # if ext pos arrives through port IN_handler
-            node_id, ext_position = inputs[self.in_handler_extpos]
-            self.subframework[node_id] = ext_position
+            node_id, external_position = inputs[self.in_handler_extpos]
+            self.subframework[node_id] = external_position
         elif self.in_handler_extact in inputs: # if ext action arrives through port IN_handler
-            ext_action += inputs[self.in_handler_extact]
+            external_action += inputs[self.in_handler_extact]
             sigma = sigma - self.elapsed # holds last status
 
         if (self.debug):
-            print("t: {:.2f} s, Atomic name: {}, External Transition Function".format(current_time,self.name))
+            print(
+                "t: {:.2f} s, Atomic name: {}, External Transition Function"
+                .format(current_time, self.name)
+            )
 
-        return ControllerState(sigma, current_time, last_position, ext_action) 
+        return ControllerState(sigma, current_time, last_position, external_action) 
     
     def intTransition(self):
         """
         Internal Transition Function.
         """
-        sigma, current_time, last_position, ext_action = self.state.get()
+        sigma, current_time, last_position, external_action = self.state.get()
         current_time += sigma
         self.action.pop()
         if len(self.action) == 0:
             sigma = self.period
             self.first_control_call = True
-            ext_action = 0
+            external_action = 0
         else:
             sigma = 0
 
         if (self.debug):
             print("t: {:.2f} s, Atomic name: {}, Internal Transition Function".format(current_time,self.name))
 
-        return ControllerState(sigma,current_time,last_position, ext_action) 
+        return ControllerState(sigma,current_time,last_position, external_action) 
     
     def outputFnc(self):
         """
         Output Funtion.
         """
-        sigma, current_time, last_position, ext_action = self.state.get()
+        sigma, current_time, last_position, external_action = self.state.get()
         if self.first_control_call:
-            int_action, ext_action = self.control_action(last_position, ext_action)
+            internal_action, external_action = self.control_action(last_position, external_action)
             self.action = [
-                {self.out_kalman_intact: int_action}, 
-                {self.out_handler_intact: ext_action},
-                {self.out_dynamics_intact: int_action}
+                {self.out_kalman_intact: internal_action}, 
+                {self.out_handler_intact: external_action},
+                {self.out_dynamics_intact: internal_action}
             ]
             self.first_control_call = False
 
@@ -154,11 +149,11 @@ class Controller(AtomicDEVS):
     def __lt__(self, other):
         return self.name < other.name
 
-    def control_action(self, position, ext_action):
+    def control_action(self, position, external_action):
         """Compute control action"""
         _, current_time, _, _ = self.state.get()
-        x = -self.radius*self.omega*np.sin(self.omega*current_time)
-        y =  self.radius*self.omega*np.cos(self.omega*current_time)
+        x = 0.0
+        y = 0.0
         # the list of outputs to be returned
         if position is None: # if Kalman filter has not yet sent an estimation of position
             # return np.array([0.0, 0.0]), {'2': np.array([-4.0, 4.0])}
