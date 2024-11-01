817c73e21c6a4c8f59ba35cc063751d0cc864866
fpresenza <francisco.presenza@gmail.com>
2024-10-31 21:12:10 -0300

    wip: add positioning system atomic

--- atomics/gpssensor.py
+++ atomics/gpssensor.py
@@ -129,3 +129,134 @@ class GPSSensor(AtomicDEVS):
     
     def evalpoly(self,p):
         return p[0]+p[1]*0 # eval poly in zero time
+
+
+import numpy as np
+
+from pypdevs.DEVS import AtomicDEVS
+from pypdevs.infinity import INFINITY
+
+# do not reset random seed
+np.random.seed(0)
+
+
+class PositioningSystemState:
+    """
+    Encapsulates the system's state
+    """
+
+    def __init__(
+            self, 
+            sigma, 
+            tvalue,
+            position
+            ):
+        """
+        Constructor (parameterizable).
+        """
+        self.set(sigma, tvalue, position)
+
+    def set(self, sigma, tvalue, position):
+        self._sigma  = sigma
+        self._tvalue = tvalue
+        self._position = position
+
+    def get(self):
+        return self._sigma, self._tvalue, self._position
+
+
+class PositioningSystem(AtomicDEVS):
+    def __init__(self, robot_id, config, name='PositioningSystem', debug=False):
+        """Atomic model for the rigidity maintenance controller"""
+
+        # Always call parent class' constructor FIRST:
+        AtomicDEVS.__init__(self, name)
+
+        # Parameters
+        self.robot_id = robot_id    # Robot identifier
+        self.period = config['period']
+        self.noise_mean = config['noise_mean']
+        self.noise_covariance = config['noise_covariance']
+
+        # STATE:
+        #  Define 'state' attribute (initial sate):
+        self.state = PositioningSystemState(
+            sigma=INFINITY, #self.period,   # waits till first token
+            tvalue=0.0, 
+            position=None
+        )
+        # ELAPSED TIME:
+        #  Initialize 'elapsed time' attribute if required
+        #  (by default, value is 0.0):
+        self.elapsed = 0.0
+
+        self.debug = debug
+
+        # PORTS:
+        #  Declare as many input and output ports as desired
+        #  (usually store returned references in local variables):
+        self.output = self.addOutPort(name="output")
+        self.input = self.addInPort(name="input")
+        
+        if (self.debug):
+            print("t: 0 s, Atomic name: {}, Init Function".format(self.name))
+
+    def extTransition(self, inputs):
+        """
+        External Transition Function.
+        """
+        sigma, current_time, _ = self.state.get()
+        current_time += self.elapsed    # NOTE: self.elapsed is always zero
+
+        position = inputs[self.input]
+        sigma = sigma - self.elapsed # holds last status
+
+        if (self.debug):
+            print(
+                "t: {:.2f} s, Atomic name: {}, External Transition Function"
+                .format(current_time, self.name)
+            )
+
+        return PositioningSystemState(sigma, current_time, position)
+    
+    def intTransition(self):
+        """
+        Internal Transition Function.
+        """
+        sigma, current_time, position = self.state.get()
+        current_time += sigma
+
+        sigma = self.period
+
+        if (self.debug):
+            print("t: {:.2f} s, Atomic name: {}, Internal Transition Function".format(current_time,self.name))
+
+        return PositioningSystemState(sigma, current_time, position) 
+    
+    def outputFnc(self):
+        """
+        Output Funtion.
+        """
+        sigma, current_time, position = self.state.get()
+
+        px = position[0][0]
+        py = position[1][0]
+        noise_sample = np.random.multivariate_normal(
+            mean=self.noise_mean.ravel(),
+            cov=self.noise_covariance
+        ).reshape(-1, 1)
+        output = np.array([px, py]) + noise_sample
+
+        return {self.output: output}
+
+    def timeAdvance(self):
+        """
+        Time-Advance Function.
+        """
+        # Compute 'ta', the time to the next scheduled internal transition,
+        # based (typically) on current State.
+        sigma, _, _ = self.state.get()
+        return sigma
+
+    def __lt__(self, other):
+        return self.name < other.name
