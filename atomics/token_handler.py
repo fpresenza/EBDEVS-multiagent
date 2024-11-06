3bda8a4af7f84afa714a9cc973943ba05fa635e9
fpresenza <francisco.presenza@gmail.com>
2024-11-06 16:54:39 -0300

    token_handler: adapt to new format + token retransmition event != token emition event

--- atomics/token_handler.py
+++ atomics/token_handler.py
@@ -1,137 +1,38 @@
 import numpy as np
 from dataclasses import dataclass
+import copy
 from pypdevs.DEVS import AtomicDEVS
 from pypdevs.infinity import INFINITY
-
-#__all__ = [
-#    'Token',
-#    'TokenHandler'
-#]
 
 
 @dataclass
 class Token(object):
     creator: str                # The robot that created it
-    kind: int                   # 0 if action, 1 if state
+    kind: str                   # action or state
     order: int                  # A counter to differentiate tokens
     data: object                # The data it carries
     hops_to_target: int         # The number of hops it must travel
     hops_travelled: int = 0     # The number of hops it has travelled
-
-class TokenGeneratorState:
-    """
-    Encapsulates the system's state
-    """
-
-    def __init__(self, sigmaval=0.1, tval=0.0, ival=0):
-        """
-        Constructor (parameterizable).
-        """
-        self.set(sigmaval, tval, ival)
-
-    def set(self, sigmavalue, tvalue, ivalue):
-        self._sigma  = sigmavalue
-        self._tvalue = tvalue
-        self._ivalue = ivalue
-
-    def get(self):
-        return self._sigma, self._tvalue, self._ivalue
-
-class TokenGenerator(AtomicDEVS):
-    def __init__(self,name=None,period=1):
-        """
-        Atomic model for the toking handling protocol
-        """
-
-        # Always call parent class' constructor FIRST:
-        AtomicDEVS.__init__(self, name)
-
-        # Parameters
-        token_0 = Token(creator='2',kind=0,order=127,data={'3': np.array([1.0, 2.0])},hops_to_target=4,hops_travelled=1)
-        token_1 = Token(creator='1',kind=0,order=127,data={'2': np.array([1.0, 2.0])},hops_to_target=4,hops_travelled=2)
-        token_2 = Token(creator='1',kind=0,order=128,data={'2': np.array([1.0, 2.0])},hops_to_target=4,hops_travelled=2)
-        token_3 = Token(creator='3',kind=1,order=132,data={'1': np.array([3.0, 4.0])},hops_to_target=4,hops_travelled=2)
-        self.tokens = [token_0, token_1, token_2, token_3]
-        self.N = len(self.tokens)
-        self.period = period
-        # STATE:
-        #  Define 'state' attribute (initial sate):
-        _time0  = 0.0
-        _sigma0 = 0
-        _i0 = 0
-        self.state = TokenGeneratorState(_sigma0,_time0,_i0) 
-        # ELAPSED TIME:
-        #  Initialize 'elapsed time' attribute if required
-        #  (by default, value is 0.0):
-        self.elapsed = 0.0
-
-        # PORTS:
-        #  Declare as many input and output ports as desired
-        #  (usually store returned references in local variables):
-        self.out_handler_token    = self.addOutPort(name="token_out")
-
-    def __lt__(self, other):
-        return self.name < other.name
-
-    def extTransition(self, inputs):
-        """
-        External Transition Function.
-        """
-        # it should never be executed
-        sigma, current_time, i = self.state.get()
-        current_time += self.elapsed
-        return TokenGeneratorState(sigma,current_time,i) 
-    
-    def intTransition(self):
-        """
-        Internal Transition Function.
-        """
-        sigma, current_time, i = self.state.get()
-        current_time += sigma
-        if i<self.N-1:
-            i += 1
-            sigma = self.period
-        else:
-            i += 1
-            sigma = INFINITY
-        return TokenGeneratorState(sigma,current_time,i) 
-    
-    def outputFnc(self):
-        """
-        Output Funtion.
-        """
-        sigma, current_time, i = self.state.get()
-        y = self.tokens[i]
-        return {self.out_handler_token: y}
-
-    def timeAdvance(self):
-        """
-        Time-Advance Function.
-        """
-        # Compute 'ta', the time to the next scheduled internal transition,
-        # based (typically) on current State.
-        sigma, current_time, i = self.state.get()
-        return sigma
 
 
 class TokenHandlerState:
     """
     Encapsulates the system's state
     """
-
-    def __init__(self, sigmaval=0.1, tval=0.0, dataval=[]):
+    def __init__(self, sigma, tvalue, history, position):
         """
         Constructor (parameterizable).
         """
-        self.set(sigmaval, tval, dataval)
+        self.set(sigma, tvalue, history, position)
 
-    def set(self, sigmavalue, tvalue, datavalue):
-        self._sigma  = sigmavalue
+    def set(self, sigma, tvalue, history, position):
+        self._sigma = sigma
         self._tvalue = tvalue
-        self._data   = datavalue
+        self._history = history
+        self._state = position
 
     def get(self):
-        return self._sigma, self._tvalue, self._data
+        return self._sigma, self._tvalue, self._history, self._state
 
 
 class TokenHandler(AtomicDEVS):
@@ -150,21 +51,23 @@ class TokenHandler(AtomicDEVS):
         # Parameters
         self.robot_id = robot_id    # Robot identifier
         self.action_extent = config['action']      # The robot's action extent
-        self.state_extent = config['state']       # The robot's state extent
+        self.state_extent = config['state']        # The robot's state extent
         # self.status = []          # TODO
         self.debug = debug
 
         # Dictionaries as records of tokens received
-        self.received = {0: {}, 1: {}}
-        self.action_token_order = 0
-        self.state_token_order = 0
 
         # STATE:
         #  Define 'state' attribute (initial sate):
-        _time0  = 0.0
-        _sigma0 = INFINITY # waits till firts token
-        _data0  = []
-        self.state = TokenHandlerState(_sigma0,_time0,_data0) 
+        self.state = TokenHandlerState(
+            sigma=INFINITY, 
+            tvalue=0.0, 
+            history={
+                'out': {'action': 0, 'state': 0}, 
+                'in': {'action': {}, 'state': {}}
+            },
+            position=None,
+        ) 
         # ELAPSED TIME:
         #  Initialize 'elapsed time' attribute if required
         #  (by default, value is 0.0):
@@ -182,6 +85,8 @@ class TokenHandler(AtomicDEVS):
         self.in_controller_intact  = self.addInPort(name="in_controller_intact")
         self.in_kalman_intpos      = self.addInPort(name="in_kalman_intpos")
 
+        self.outputs_queue = []
+
         if (self.debug):
             print("t: 0 s, Atomic name: {}, Init Function".format(self.name))
 
@@ -189,79 +94,102 @@ class TokenHandler(AtomicDEVS):
         """
         External Transition Function.
         """
-        sigma, current_time, data = self.state.get()
+        sigma, current_time, history, position = self.state.get()
         current_time += self.elapsed
 
-        if self.in_router_token in inputs: # if token arrives through port in_router_token
-            token, distance_meas = inputs[self.in_router_token]
-            data_received = self.handle_received_token(token, distance_meas) # events list
-            if (data_received == []): # pass, nothing to send
-                sigma = sigma - self.elapsed # holds last status
-            else:
-                data += data_received # events list
+        if self.in_router_token in inputs:    # if token arrives through port in_router_token
+            token, distance_measurement = inputs[self.in_router_token]
+            history, msgs = self.handle_received_token(
+                history,
+                token,
+                distance_measurement
+            )
+
+            if len(msgs) > 0:    # else pass, nothing to send
+                self.outputs_queue += msgs
                 sigma = 0.0
+    
             if (self.debug):
-                print("t: {} s, Atomic name: {}, External Transition Function, token: {} from Router".format(current_time,self.name,token))
+                print(
+                    "t: {} s, Atomic name: {}, External Transition Function, token: {} from Router"
+                    .format(current_time, self.name, token)
+                )
 
         elif self.in_controller_intact in inputs: # if data arrives through port in_controller_intact
-            token = Token(
+            action_token = Token(
                 creator=self.parent.name,
-                kind=0,
-                order=self.action_token_order,
+                kind='action',
+                order=history['out']['action'],
                 data=inputs[self.in_controller_intact],
                 hops_to_target=self.action_extent,
                 hops_travelled=0
             )
-            data.append({self.out_router_token: token})
-            self.action_token_order+=1
+            history['out']['action'] += 1
+            self.outputs_queue.append({self.out_router_token: action_token})
+
+            if position is not None:
+                state_token = Token(
+                    creator=self.parent.name,
+                    kind='state',
+                    order=history['out']['state'],
+                    data=position,
+                    hops_to_target=self.state_extent,
+                    hops_travelled=0
+                )
+                history['out']['state'] += 1
+                self.outputs_queue.append({self.out_router_token: state_token})
 
             sigma = 0.0
+
             if (self.debug):
-                print("t: {} s, Atomic name: {}@{}, External Transition Function, token: {} from Controller".format(current_time,self.name,self.parent.name,token))
+                print(
+                    "t: {} s, Atomic name: {}@{}, External Transition Function, token: {} from Controller"
+                    .format(current_time, self.name, self.parent.name, action_token)
+                )
 
         elif self.in_kalman_intpos in inputs:   # if data arrives through port in_kalman_intpos
-            token = Token(
-                creator=self.parent.name,
-                kind=1,
-                order=self.state_token_order,
-                data=inputs[self.in_kalman_intpos],
-                hops_to_target=self.state_extent,
-                hops_travelled=0
-            )
-            data.append({self.out_router_token: token})
-            self.state_token_order+=1
+            position = inputs[self.in_kalman_intpos]
 
-            sigma = 0.0
             if (self.debug):
-                print("t: {} s, Atomic name: {}@{}, External Transition Function, token: {} from Kalman".format(current_time,self.name,self.parent.name,token))
+                print(
+                    "t: {} s, Atomic name: {}@{}, External Transition Function, token: {} from Kalman"
+                    .format(current_time, self.name, self.parent.name, token)
+                )
 
-        return TokenHandlerState(sigma,current_time,data) 
+        return TokenHandlerState(sigma, current_time, history, position) 
     
     def intTransition(self):
         """
         Internal Transition Function.
         """
-        _, current_time, data = self.state.get()
+        _, current_time, history, position = self.state.get()
         
-        data.pop(0)
-        if len(data) == 0:
+        if len(self.outputs_queue) == 0:
             sigma = INFINITY
         else:
             sigma = 0.0
 
         if (self.debug):
-            print("t: {} s, Atomic name: {}@{}, Internal Transition Function".format(current_time,self.name,self.parent.name))
+            print(
+                "t: {} s, Atomic name: {}@{}, Internal Transition Function"
+                .format(current_time, self.name, self.parent.name)
+            )
 
-        return TokenHandlerState(sigma,current_time,data)
+        return TokenHandlerState(sigma, current_time, history, position)
     
     def outputFnc(self):
         """
         Output Funtion.
         """
-        sigma, current_time, data = self.state.get()
+        _, current_time, _, _ = self.state.get()
+
         if (self.debug):
-            print("t: {} s, Atomic name: {}@{}, Output Function, data: {}".format(current_time,self.name,self.parent.name, data[0]))
-        return data[0]
+            print(
+                "t: {} s, Atomic name: {}@{}, Output Function, data: {}"
+                .format(current_time, self.name, self.parent.name, self.outputs_queue[0])
+            )
+        
+        return self.outputs_queue.pop(0)
     
 
     def timeAdvance(self):
@@ -270,59 +198,59 @@ class TokenHandler(AtomicDEVS):
         """
         # Compute 'ta', the time to the next scheduled internal transition,
         # based (typically) on current State.
-        sigma, _, _ = self.state.get()
+        sigma, _, _, _ = self.state.get()
         return sigma
     
     def __lt__(self, other):
         return self.name < other.name
 
-    def handle_received_token(self, token, distance):
+    def handle_received_token(self, history, token, distance):
         """Decide what to do with the received token"""
-        # the list of outputs to be returned
-        outputs = []
+        msgs = []
 
         if token.creator == self.robot_id:
             # do nothing if this robot is the creator
             pass
         else:
             # update the number of traversed hops
+            token = copy.deepcopy(token)
             token.hops_travelled += 1
 
             # check if retransmission is needed
             if token.hops_travelled < token.hops_to_target:
-                outputs.append({self.out_router_token: token})
+                msgs.append({self.out_router_token: token})
 
             try:
                 # gets order from received dictionary
-                order = self.received[token.kind][token.creator]
+                last_order = history['in'][token.kind][token.creator]
             except KeyError:
                 # first time received
-                order = -1
+                last_order = -1
 
             # check if token is newer than last received
-            if token.order > order:
-                self.received[token.kind][token.creator] = token.order
+            if token.order > last_order:
+                history['in'][token.kind][token.creator] = token.order
                 # check if token is of kind action
-                if token.kind == 0:
+                if token.kind == 'action':
                     try:
                         # check if there is data for this robot
                         data = (token.creator, token.data[self.robot_id])
                         # send data to controller
-                        outputs.append({self.out_controller_extact: data})
+                        msgs.append({self.out_controller_extact: data})
                     except KeyError:
                         pass
                 # check if token is of kind state
-                elif token.kind == 1:
+                elif token.kind == 'state':
                     # check if token creator is within action extent
                     if token.hops_travelled <= self.action_extent:
                         # send data to controller
                         data = (token.creator, token.data)
-                        outputs.append({self.out_controller_extpos: data})
+                        msgs.append({self.out_controller_extpos: data})
                         if token.hops_travelled == 1:
                             # send data to positioning system
                             data += (distance,)
-                            outputs.append({self.out_kalman_extpos: data})
+                            msgs.append({self.out_kalman_extpos: data})
 
-        return outputs
+        return history, msgs
 
 
