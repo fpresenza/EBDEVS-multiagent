fcf3ba240ac565540754a471d8dadb5aa99de4cd
fpresenza <francisco.presenza@gmail.com>
2024-11-20 13:02:36 -0300

    wip: core

--- utils/core.py
+++ utils/core.py
@@ -1,4 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 def robot_id_to_index(robot_id):
     underscore = robot_id.rindex('_')
     return int(robot_id[underscore + 1:])
+
+
+def target_id_to_index(target_id):
+    underscore = target_id.rindex('_')
+    return int(target_id[underscore + 1:])
