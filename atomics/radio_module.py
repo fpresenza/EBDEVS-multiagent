3e53cca3bff599f670f16157e41e00f15913f617
fpresenza <francisco.presenza@gmail.com>
2024-11-25 19:52:58 -0300

    minor fix

--- atomics/radio_module.py
+++ atomics/radio_module.py
@@ -78,7 +78,7 @@ class RadioModule(AtomicDEVS):
         if self.inPorts['radio'] in inputs:
             transmitter, msg = inputs[self.inPorts['radio']]
 
-            if transmitter.startswith('Robot') and (msg[0] is not None):
+            if transmitter.startswith('Robot'):
                 self.outputs_queue.append({self.outPorts['token']: msg})
                 sigma = 0.0
             elif transmitter.startswith('Target'):
