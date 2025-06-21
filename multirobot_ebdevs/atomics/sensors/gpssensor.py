46cb9a8949071352e72e96337a263efdf5db0a04
fpresenza <francisco.presenza@gmail.com>
2025-06-21 16:29:53 -0300

    minor change gps

--- multirobot_ebdevs/atomics/sensors/gpssensor.py
+++ multirobot_ebdevs/atomics/sensors/gpssensor.py
@@ -12,10 +12,8 @@ class GPSSensor(ExteroceptiveSensor):
         #  compute measurement here
         #
         p = self.parent.getRobotPosition(current_time)
-        px = p[0]
-        py = p[1]
         noise_sample = np.random.multivariate_normal(
             mean=self.noise_mean.ravel(),
             cov=self.noise_covariance
         )
-        return [px + noise_sample[0], py + noise_sample[1]]
+        return [p[0] + noise_sample[0], p[1] + noise_sample[1]]
